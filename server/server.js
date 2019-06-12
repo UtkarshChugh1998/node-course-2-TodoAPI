4a6ebfd547092c6df2f78ab5f02c472f3d7c6bf9
Utkarsh Chugh <utkarshchugh98@gmail.com>
2019-06-12 18:07:40 +0530

    Adding GET /todos route

--- server/server.js
+++ server/server.js
@@ -11,11 +11,14 @@ app.use(bodyParser.json());
 // Creating new Todo
 // Handling the post request to '/todos' route
 // Since we have
+
 app.post('/todos',(req,res)=>{
-	var todo =new Todo({
+	// If an object failing the properties' conditions is defined
+	// then new Todo() will not throw an error but, .save() will throw 
+	// one
+	var todo = new Todo({
 		text: req.body.text
 	});
-
 
 	todo.save().then((doc)=>{
 		res.send(doc);
@@ -23,8 +26,29 @@ app.post('/todos',(req,res)=>{
 		res.status(400).send(e);
 	})
 });
+
+// Reading all resourses(TOdos)
+app.get('/todos',(req, res)=>{
+	Todo.find().then((todos)=>{
+		// While passing an array, we cannot pass
+		// other res properties like custom code
+		// Better method is to pass an array
+		// with todos array inside as key value pair
+		res.send({todos,
+			code: 'asas'});
+	}).catch((e)=>
+	{
+		res.status(400).send(e);
+	})
+})
+
+
 // Making our web server listen to some port for route requests
 // Then forward those requests to the app server to handle them
 app.listen(3000, ()=>{
 	console.log(' Web Server running on port 3000');
 })
+
+module.exports = {
+	app
+}
